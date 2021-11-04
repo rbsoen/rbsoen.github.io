c5751bd3fa088804cc0ceee5e7b0cebe4be70be5
R Breil Soenoto <rbsoenoto@gmail.com>
2021-11-04 16:11:39 +0700

    remove unneeded fontawesome include

--- index.src.php
+++ index.src.php
@@ -11,7 +11,6 @@ require_once "featured_works.php"
 		<meta charset="utf-8">
 		<title>rbsoen</title>
 		<link rel="stylesheet" href="style.css" media="screen">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous">
 		<meta name="viewport" content="width=device-width,height=device-height,initial-scale=1">
 	</head>
 	<body>
